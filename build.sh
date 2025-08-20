2d991ea7676d0fcd5d5251b3e8890872ceaa15aa
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2025-08-20 20:57:42 +0800

    Move all build logics to build.sh
    Start command now will be OLLAMA_DEBUG=DEBUG OLLAMA_ORIGINS="*" OLLAMA_HOST=0.0.0.0:$PORT $HOME/homebrew/bin/ollama serve

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+
+git clone https://github.com/Homebrew/brew homebrew
+
+eval "$(homebrew/bin/brew shellenv)"
+brew update --force --quiet
+chmod -R go-w "$(brew --prefix)/share/zsh"
+
+brew install ollama
