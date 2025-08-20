c1b5f8104c2f1e4641c4cce6b0a140c751e1f41d
HananiahKao <168612859+HananiahKao@users.noreply.github.com>
2025-08-20 18:50:48 +0800

    Add run.sh as start command on Render
    run.sh downloads Homebrew in the PWD and install Ollama with Homebrew.

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+
+git clone https://github.com/Homebrew/brew homebrew
+
+eval "$(homebrew/bin/brew shellenv)"
+brew update --force --quiet
+chmod -R go-w "$(brew --prefix)/share/zsh"
+
+brew install ollama
+
+ollama serve
+
+./app
